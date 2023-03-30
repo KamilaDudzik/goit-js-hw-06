c06d9e8f8ec887fb55152aebcd4366617cc90598
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-03-30 10:29:36 +0200

    First build of task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector(".login-form");
+const emailInput = loginForm.elements[0];
+const passwordInput = loginForm.elements[1];
+const button = loginForm.elements[2];
 
+const handleSubmit = (event) => {
+  // event.preventDefault();
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please fill in all of the fields!");
+  } else {
+    const userInfo = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+
+    console.log(userInfo.email);
+    console.log(userInfo.password);
+    loginForm.reset();
+  }
+};
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  handleSubmit(e);
+});
+
+// loginForm.addEventListener("submit", handleSubmit);
