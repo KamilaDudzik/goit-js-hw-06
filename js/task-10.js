fded83d87a8df35853dc67945083f95449bf1b7d
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-03-30 17:41:29 +0200

    First build of task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBoxes = document.getElementById("boxes");
+const inputEl = document.querySelector("input");
+
+let newHeight = 20;
+let newWidth = 20;
+
+const buttonCreate = document.querySelector("[data-create");
+
+const createBoxes = (amount) => {
+  for (let i = 0; i < amount; i++) {
+    const newBox = document.createElement("div");
+    newBox.style.height = (newHeight += 10) + "px";
+    newBox.style.width = (newWidth += 10) + "px";
+    newBox.style.backgroundColor = getRandomHexColor();
+
+    divBoxes.append(newBox);
+  };
+};
+
+buttonCreate.addEventListener("click", (eventCreate) => {
+  eventCreate.preventDefault();
+  createBoxes(inputEl.value);
+});
+
+const buttonDestroy = document.querySelector("[data-destroy]");
+
+buttonDestroy.addEventListener("click", () => {
+  divBoxes.remove(divBoxes.childNodes);
+  location.reload();
+});
