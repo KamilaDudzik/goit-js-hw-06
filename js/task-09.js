0dca37522b13bb92909f375c09762a3ce118a13a
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-03-30 10:57:31 +0200

    First build of task-09

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,10 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+});
